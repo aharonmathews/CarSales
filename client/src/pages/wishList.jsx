007e30e7689be5892e245ff6d07130ddac768474
Ivin <ivinmk2410@hotmail.com>
2024-09-19 19:18:16 +0530

    minor change

--- client/src/pages/wishList.jsx
+++ client/src/pages/wishList.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { WishlistContext } from "./WishlistContext"; 
 const Wishlist = () => {
   const [wishlistItems, setWishlistItems] = useState([
     {
